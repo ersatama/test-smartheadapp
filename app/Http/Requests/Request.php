ca6e18398e2baf74394f9fd18b256f1f270f63f6
ersatama <ersatama@gmail.com>
2025-10-15 15:40:10 +0500

    feat: added request error validation trait file

---
+++ app/Http/Requests/Request.php
@@ -0,0 +1,34 @@
+<?php
+
+namespace App\Http\Requests;
+
+use Illuminate\Contracts\Validation\Validator;
+use Illuminate\Http\Exceptions\HttpResponseException;
+use Illuminate\Validation\ValidationException;
+
+trait Request
+{
+    /**
+     * Determine if the user is authorized to make this request.
+     */
+    public function authorize(): bool
+    {
+        return true;
+    }
+
+    /**
+     * @throws ValidationException
+     */
+    public function checked(): array
+    {
+        return $this->validator->validated();
+    }
+
+    protected function failedValidation(Validator $validator)
+    {
+        throw new HttpResponseException(response()->json([
+            'status'        =>  'failure',
+            'errors'        =>  $validator->errors(),
+        ], 400));
+    }
+}
