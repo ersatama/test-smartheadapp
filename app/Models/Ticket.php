11bbbd6ee8fdeda44ca34e50419074cf33809deb
ersatama <ersatama@gmail.com>
2025-10-15 13:02:30 +0500

    feat: added models Customer, Ticket, User with relations

---
+++ app/Models/Ticket.php
@@ -0,0 +1,57 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Relations\BelongsTo;
+use Illuminate\Database\Eloquent\Relations\MorphMany;
+use Illuminate\Database\Eloquent\Builder;
+use Spatie\MediaLibrary\HasMedia;
+use Spatie\MediaLibrary\InteractsWithMedia;
+use Spatie\MediaLibrary\MediaCollections\Models\Media;
+
+class Ticket extends Model implements HasMedia
+{
+    use HasFactory, InteractsWithMedia;
+
+    protected $fillable = [
+        'customer_id',
+        'subject',
+        'text',
+        'status',
+        'manager_reply_at',
+    ];
+
+    public function customer(): BelongsTo
+    {
+        return $this->belongsTo(Customer::class);
+    }
+
+    public function media(): MorphMany
+    {
+        return $this->morphMany(Media::class, 'model');
+    }
+
+    public function scopeCreatedToday(Builder $query): Builder
+    {
+        return $query->whereDate('created_at', now()->toDateString());
+    }
+
+    public function scopeCreatedThisWeek(Builder $query): Builder
+    {
+        return $query->whereBetween('created_at', [now()->startOfWeek(), now()->endOfWeek()]);
+    }
+
+    public function scopeCreatedThisMonth(Builder $query): Builder
+    {
+        return $query->whereMonth('created_at', now()->month);
+    }
+
+    public function registerMediaCollections(): void
+    {
+        $this->addMediaCollection('attachments')
+            ->useDisk('public')
+            ->singleFile(false);
+    }
+}
