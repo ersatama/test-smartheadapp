76549adfd061268d7ab8dabba51373ffef07fd60
ersatama <ersatama@gmail.com>
2025-10-15 13:09:18 +0500

    feat: added seeders with test data

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -2,24 +2,38 @@
 
 namespace Database\Seeders;
 
+use App\Models\Customer;
+use App\Models\Ticket;
 use App\Models\User;
-use Illuminate\Database\Console\Seeds\WithoutModelEvents;
 use Illuminate\Database\Seeder;
+use Spatie\Permission\Models\Role;
 
 class DatabaseSeeder extends Seeder
 {
-    use WithoutModelEvents;
-
     /**
      * Seed the application's database.
      */
     public function run(): void
     {
-        // User::factory(10)->create();
+        Role::create(['name' => 'admin']);
+        Role::create(['name' => 'manager']);
 
-        User::factory()->create([
-            'name' => 'Test User',
-            'email' => 'test@example.com',
+        $admin = User::factory()->create([
+            'name' => 'Admin',
+            'email' => 'admin@example.com',
+            'password' => bcrypt('password'),
         ]);
+        $admin->assignRole('admin');
+
+        $manager = User::factory()->create([
+            'name' => 'Manager',
+            'email' => 'manager@example.com',
+            'password' => bcrypt('password'),
+        ]);
+        $manager->assignRole('manager');
+
+        Customer::factory(5)
+            ->has(Ticket::factory(3))
+            ->create();
     }
 }
